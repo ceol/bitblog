e45f750f552fea7d20b5f9fb063233ef4fdb7463
Patrick Jacobs <ceolwulf@gmail.com>
2012-11-18 21:33:27 -0500

    started post_edit view, added form to new_post

--- bitblog/views.py
+++ bitblog/views.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, render_template, request, url_for, abort
+from flask import flash, redirect, render_template, abort
 from bitblog import app
 from bitblog.database import db_session
 from bitblog.models import Post
@@ -10,7 +10,8 @@ def index():
 
 @app.route('/new')
 def new_post():
-    return render_template('new_post.html')
+    form = PostForm()
+    return render_template('new_post.html', form=form)
 
 @app.route('/<regex("[A-Za-z0-9_-]"):slug>')
 def post_detail(slug):
@@ -18,3 +19,11 @@ def post_detail(slug):
     if post is None:
         abort(404)
     return render_template('post_detail.html', post=post)
+
+@app.route('/<regex("[A-Za-z0-9_-]"):slug>/edit')
+def post_edit(slug):
+    post = Post.query.filter_by(slug=slug).first()
+    if post is None:
+        abort(404)
+    form = PostForm()
+    return render_template('post_edit.html', form=form)
