1893cd28c1fd35253130a077a3c8c4b405297e3f
Patrick Jacobs <ceolwulf@gmail.com>
2012-11-18 21:20:59 -0500

    added outline of views, post detail

--- bitblog/views.py
+++ bitblog/views.py
@@ -1,6 +1,20 @@
-from flask import flash, redirect, render_template, request, url_for
+from flask import flash, redirect, render_template, request, url_for, abort
 from bitblog import app
 from bitblog.database import db_session
 from bitblog.models import Post
 from bitblog.forms import PostForm
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/new')
+def new_post():
+    return render_template('new_post.html')
+
+@app.route('/<regex("[A-Za-z0-9_-]"):slug>')
+def post_detail(slug):
+    post = Post.query.filter_by(slug=slug).first()
+    if post is None:
+        abort(404)
+    return render_template('post_detail.html', post=post)
