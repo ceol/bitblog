11c32581e452499802fa55307d751ea18b7061e1
Patrick Jacobs <ceolwulf@gmail.com>
2012-11-18 20:50:03 -0500

    added Post model

--- bitblog/models.py
+++ bitblog/models.py
@@ -7,5 +7,18 @@ from sqlalchemy import (
     Text,
     String,
 )
-from sqlalchemy.orm import relationship, backref
+#from sqlalchemy.orm import relationship, backref
 from bitblog.database import Base
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True)
+
+    created = Column(DateTime, default=func.now())
+
+    slug = Column(String, unique=True)
+    title = Column(String)
+    content = Column(Text)
+
+    is_published = Column(Boolean)
